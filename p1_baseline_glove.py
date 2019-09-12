450d125153b3841b886c8d347fef3f8fbe712cf8
coderTazz <vdvinayak@gmail.com>
2019-09-12 01:12:53 -0400

    linear 50%, fixed bracket picking

--- p1_baseline_glove.py
+++ p1_baseline_glove.py
@@ -5,10 +5,7 @@ import nltk
 from nltk.tokenize import RegexpTokenizer
 import numpy
 import sklearn
-from sklearn.linear_model import SGDClassifier
 from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
-# from sklearn.pipeline import Pipeline
 from sklearn import svm
 
 
@@ -20,14 +17,14 @@ def trainAndTestBaseline(trainData, dfTrain):
 
 	# print(dfTrain['Event 1'][1].shape)
 
-	causal_classify = svm.SVC(kernel = 'poly', degree = 4, decision_function_shape = 'ovr')
+	causal_classify = svm.SVC(kernel = 'linear')
 	_ = causal_classify.fit(trainData, dfTrain['Label'])
 	print('Training Complete')
 
 	# Load and Process Test Data
 	dfTestRaw = pd.read_csv("P1_testing_set.csv", usecols = fields)
 	print('Test set loaded')
-	dfTestToken = tokenizeE1E2AndStore(dfTestRaw, False)
+	dfTestToken = tokenizeE1E2AndStore(dfTestRaw)
 	print('Test set tokenized')
 	testData,_ = embedAndConcatenate(dfTestToken)
 	print('Test set vectors formed and concatenated')
@@ -54,15 +51,15 @@ def embedAndConcatenate(df):
 		
 		# Event1
 		x = numpy.zeros(len(nlp.vocab['dog'].vector,))
-		for j in range(0, len(df['Event 1'].iloc(0)[i])):
-			x = x + nlp.vocab[df['Event 1'].iloc(0)[i][j]].vector
-		x = x/len(df['Event 1'].iloc(0)[i])
+		for j in range(0, len(df['Event 1'][i])):
+			x = x + nlp.vocab[df['Event 1'][i][j]].vector
+		x = x/len(df['Event 1'][i])
 
 		# Event2
 		y = numpy.zeros(len(nlp.vocab['dog'].vector,))
-		for j in range(0, len(df['Event 2'].iloc(0)[i])):
-			y = y + nlp.vocab[df['Event 2'].iloc(0)[i][j]].vector
-		y = y/len(df['Event 2'].iloc(0)[i])
+		for j in range(0, len(df['Event 2'][i])):
+			y = y + nlp.vocab[df['Event 2'][i][j]].vector
+		y = y/len(df['Event 2'][i])
 
 		# Concatenate the two events and store
 		x = numpy.concatenate((x, y))
@@ -76,29 +73,26 @@ def embedAndConcatenate(df):
 
 
 
-def tokenizeE1E2AndStore(dfRaw, storeBool):
+def tokenizeE1E2AndStore(df):
 	tokenizer = RegexpTokenizer(r'\w(?:[-\w]*\w)?')
-	dfRaw['Event 1'] = dfRaw['Event 1'].apply(tokenizer.tokenize)
-	dfRaw['Event 2'] = dfRaw['Event 2'].apply(tokenizer.tokenize)
-	if storeBool:
-		dfRaw.to_csv('Tokenized_Small_Train_Expanded_Set.csv')
-	else:
-		return dfRaw
+	df['Event 1'] = df['Event 1'].apply(tokenizer.tokenize)
+	df['Event 2'] = df['Event 2'].apply(tokenizer.tokenize)
+	
+	return df
 
 
 
 def main():
 	# Loading Training Data
-	# dfRaw = pd.read_csv("train_expanded.csv", usecols = fields)
-	# tokenizeE1E2AndStore(dfRaw, True)
+	dfRaw = pd.read_csv("P1_training_set.csv", usecols = fields)
+	dfTToken = tokenizeE1E2AndStore(dfRaw)
 	print('Training Set Tokenized')
-	dfTToken = pd.read_csv("Tokenized_Small_Train_Expanded_Set.csv", usecols = fields)
+	# dfTToken = pd.read_csv("Tokenized_Small_Train_Expanded_Set.csv", usecols = fields)
 	trainData, dfTrain = embedAndConcatenate(dfTToken)
 	print('Train set vectors formed and concatenated')
 	# print(trainData.shape)
 	print('Proceeding for Training')
 	prediction_result = trainAndTestBaseline(trainData, dfTrain)
-
 
 
 
