abf6b86bdb43b9e12f92ed18a8dff9985f6eed35
coderTazz <vdvinayak@gmail.com>
2019-09-14 15:03:48 -0400

    baseline w2v SVM classifier, 50.77% acc

--- p1_w2v.py
+++ p1_w2v.py
@@ -7,6 +7,9 @@ import numpy as np
 import sklearn
 from sklearn.svm import SVC
 from sklearn import svm
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
 
 
 
@@ -23,11 +26,18 @@ def trainAndTestBaseline(trainData, testData, dfTrain, dfTest):
 
 	# Load and Process Test Data
 	prediction_result = causal_classify.predict(testData)
+	print()
 	print('Result')
-	print(round(100*np.mean(prediction_result == dfTest['Label']), 2))
-	# print(confusion_matrix(dfTestRaw['Label'], prediction_result))
+	print("Accuracy: " + str(round(100*np.mean(prediction_result == dfTest['Label']), 2)) + "%")
+
+	print("Recall: " + str(round(100*recall_score(dfTest['Label'], prediction_result, average='micro'), 2)) + "%")
+	print("Precision: " + str(round(100*precision_score(dfTest['Label'], prediction_result, average='micro'), 2)) + "%")
 	# for i in range(0, prediction_result.shape[0]):
 	# 	print(str(prediction_result[i]) + " " + str(dfTestToken['Label'][i]))
+
+	cm = confusion_matrix(prediction_result, dfTest['Label'])
+	print("Confusion Matrix: ")
+	print(cm)
 
 	return prediction_result
 
@@ -39,7 +49,7 @@ def trainAndTestBaseline(trainData, testData, dfTrain, dfTest):
 def main():
 	# Loading Training Data
 	dfTrainRaw = pd.read_csv("train_expanded.csv", usecols = fields)
-	dfTestRaw = pd.read_csv("P1_testing_set.csv", usecols = fields)
+	dfTestRaw = pd.read_csv("test_expanded.csv", usecols = fields)
 	trainData = np.loadtxt('../w2v_Averaged_Expanded_Train.csv', delimiter = ',')
 	testData = np.loadtxt('../w2v_Averaged_Expanded_Test.csv', delimiter = ',')
 	# print(trainData.shape, testData.shape)
